05cd980dcbd326f88e3beba117581fe372681081
Andrey Ageev <andrey-ageev-mail@yandex.ru>
2019-11-27 00:46:47 +0600

    Replaced the show() to fadeIn() - Заменил show () на fadeIn ()

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ $(document).ready(function(){
 
     $('.about__toggle').click(function(event){
         event.preventDefault();
-        console.log('click');
 
         $('.about-content__box').hide();
 
         var href = $(this).attr('href');
-        console.log(href);
 
-        $(href).show();
+        $(href).fadeIn();
     });
 
 });
