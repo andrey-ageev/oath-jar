165b8d2f93885b377e3a499f771dfe42743547a5
Andrey Ageev <andrey-ageev-mail@yandex.ru>
2019-11-27 00:33:37 +0600

    Added a script to get the name of an attribute of "href" - Добавлен скрипт для получения имени атрибута "href"

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
         console.log('click');
 
         $('.about-content__box').hide();
+
+        var href = $(this).attr('href');
+        console.log(href);
     });
 
 });
